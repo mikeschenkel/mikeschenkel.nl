753e215db1b573d5096d8033fe8540bd7bea4398
Mike Schenkel <mikeschenkel@gmail.com>
2019-03-19 22:57:09 +0100

    Add aria-label attribute option

--- _11ty/shortcodes.js
+++ _11ty/shortcodes.js
@@ -1,11 +1,21 @@
 module.exports = {
   /**
    * Wrap an emoji with accessible markup.
-   * https://tink.uk/accessible-emoji/
+   * {@link https://tink.uk/accessible-emoji/}
    *
-   * @param {String} emoji
+   * @example
+   * {% emoji '🔥' %}
+   * {% emoji '😀', 'Happy Face' %}
+   *
+   * @param {string} emoji
+   * @param {string} ariaLabel
    */
-  emoji: function(emoji) {
-    return `<span aria-hidden="true" role="img">${emoji}</span>`;
+  emoji: function(emoji, ariaLabel) {
+    let ariaAttr = `aria-hidden="true"`;
+    if (ariaLabel) {
+      ariaAttr = `aria-label="${ariaLabel}"`;
+    }
+
+    return `<span role="img" ${ariaAttr}>${emoji}</span>`;
   }
 };
