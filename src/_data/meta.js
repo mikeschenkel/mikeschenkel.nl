1136bd4efa870ef9be900cd1b612ffdd63296dc4
Mike Schenkel <mikeschenkel@gmail.com>
2022-08-30 01:05:16 +0200

    Update meta data

--- src/_data/meta.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	siteName: "Mike Schenkel",
-	siteDescription: "De persoonlijke website van Mike Schenkel",
-	siteLang: "nl",
-	siteLocale: "nl_NL",
-	authorName: "Mike Schenkel",
-	twitterUsername: "mikeschenkel",
-};
