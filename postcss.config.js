01410a72bac02f17b4a036102878294970bf51e2
Mike Schenkel <mikeschenkel@gmail.com>
2022-09-22 00:01:33 +0200

    Use Lightning CSS

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,18 @@ module.exports = {
 	plugins: [
 		require('postcss-import-ext-glob'),
 		require('postcss-import'),
-
-		require('autoprefixer'),
-		require('postcss-custom-media'),
-		require('postcss-media-minmax'),
-		require('postcss-nesting'),
-
-		...(process.env.NODE_ENV === "production" ? [require("cssnano")] : []),
+		require('postcss-lightningcss')({
+			browsers: [
+				"> 0.25%",
+				"not IE 11"
+			],
+			lightningcssOptions: {
+				minify: (process.env.NODE_ENV === "production" ? true : false),
+				drafts: {
+					customMedia: true,
+					nesting: true,
+				},
+			},
+		}),
 	]
 };
